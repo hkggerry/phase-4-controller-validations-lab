d2935f8c0b2d3307876f261a5eb0ca87b8ffd0d0
gerry <hkggerry@gmail.com>
2022-07-17 21:34:25 -0700

    commit tree 42f301dc79a0c3ec44b7ca70486b6e158dce74a2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
 end
